93d764b845356ec3589a6f90ca3e408ad6b5e29e
Ben Clyde Sikanwe <151398987+RockingWithTheBest@users.noreply.github.com>
2026-03-31 16:51:10 +0300

    feat : Making of the second section adaptive

--- Mobile Application/Components/Code-of-conduct/code-of-conduct-full.js
+++ Mobile Application/Components/Code-of-conduct/code-of-conduct-full.js
@@ -1,14 +1,3 @@
-const backToTop = document.getElementById('backToTop')
-
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 300){
-        backToTop.classList.add('show')
-    }
-    else{
-        backToTop.classList.remove('show')
-    }
-})
-
 // Smooth Scrolling for Anchor Links
 document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
     anchor.addEventListener('click', function(e){
